2a4856dcd04a613a24e2a7d0c211a260fb0fe243
Rakesh Karmaker <rakeshkarmaker0175@gmail.com>
2025-12-16 15:48:35 +0600

    Refactor UDP client and server code for gamepad input handling; unify socket variable naming and enhance data transmission logic

--- client.py
+++ client.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 import vgamepad as vg
 from inputs import get_gamepad
+from controller.gamepad import apply_payload_to_gamepad
+from controller.state import current_axis_values
 
 # Load variables from .env into environment
 load_dotenv()
@@ -11,13 +13,19 @@ SERVER_IP_ADDRESS = os.getenv('SERVER_IP_ADDRESS', '')
 SERVER_PORT = int(os.getenv('SERVER_PORT', '5000'))
 
 # Create a UDP socket
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # initialize the virtual gamepad
 gamepad = vg.VX360Gamepad()
 
 while True:
+    # send the user's virtual gamepad input to the server
     events = get_gamepad()
     for event in events:
         controller_input = f"{event.ev_type},{event.code},{event.state}"
-        clientSocket.sendto(controller_input.encode(), (SERVER_IP_ADDRESS, SERVER_PORT))
+        client_socket.sendto(controller_input.encode(), (SERVER_IP_ADDRESS, SERVER_PORT))
+    
+    # Receive controllerInput from server
+    payload, server_address = client_socket.recvfrom(4096) # buffer size is 4096 bytes
+    print(f"Received payload: {payload.decode()} from {server_address}")
+    apply_payload_to_gamepad(gamepad, payload.decode(), current_axis_values)
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import sys
 import vgamepad as vg
 from controller.gamepad import apply_payload_to_gamepad
 from controller.state import current_axis_values
+from inputs import get_gamepad
 
 # Load variables from .env into environment
 load_dotenv()
@@ -16,21 +17,25 @@ SERVER_PORT = int(os.getenv('SERVER_PORT', '5000'))
 gamepad = vg.VX360Gamepad()
 
 # Create a UDP socket
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Bind the socket to the address and port
-serverSocket.bind((SERVER_IP_ADDRESS, SERVER_PORT))
+server_socket.bind((SERVER_IP_ADDRESS, SERVER_PORT))
 
 try:
     while True:
         # Receive controllerInput from client
-        payload, clientAddress = serverSocket.recvfrom(2048) # buffer size is 2048 bytes
-        print(f"Received payload: {payload.decode()} from {clientAddress}")
+        payload, client_address = server_socket.recvfrom(2048) # buffer size is 2048 bytes
+        print(f"Received payload: {payload.decode()} from {client_address}")
         apply_payload_to_gamepad(gamepad, payload.decode(), current_axis_values)
 
-        
+        # send the server controllerInput to the user's virtual gamepad
+        events = get_gamepad()
+        for event in events:
+            controller_input = f"{event.ev_type},{event.code},{event.state}"
+            server_socket.sendto(controller_input.encode(), client_address)
 
 except KeyboardInterrupt:
     print("Server is shutting down.")
-    serverSocket.close()
+    server_socket.close()
     sys.exit(0)
