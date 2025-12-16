31797cc464bb39cabe0ef5b9359e49fd7a796b40
Rakesh Karmaker <rakeshkarmaker0175@gmail.com>
2025-12-16 16:50:01 +0600

    Remove unused gamepad initialization and imports from client.py

--- client.py
+++ client.py
@@ -3,8 +3,6 @@ import os
 from dotenv import load_dotenv
 import vgamepad as vg
 from inputs import get_gamepad
-from controller.gamepad import apply_payload_to_gamepad
-from controller.state import current_axis_values
 
 # Load variables from .env into environment
 load_dotenv()
@@ -16,9 +14,6 @@ print(f"Connecting to server at {REMOTE_SERVER_IP}:{REMOTE_SERVER_PORT}")
 
 # Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# initialize the virtual gamepad
-gamepad = vg.VX360Gamepad()
 
 while True:
     # send the user's virtual gamepad input to the server
